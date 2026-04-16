66cb678ef0d43f23ece4de5d7b1aa4b690609c53
Ester Assis <esterassissantos4@gmail.com>
2026-04-16 14:33:15 -0300

    Estou começando do zero, espero muito aprender algo!

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 #Git curso - Udemy
 
 Estou também usando a IA ChatGPT pra aprender melhor.
+
+Comecei o curso no dia 13/04/2026. Tive que começar tudo do zero pois não estava gostando dos resultados.Então essa é a versão 2.0 ESpero muito que agora eu consiga aprender de fato!
